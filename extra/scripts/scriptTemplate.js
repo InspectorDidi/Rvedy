95403a372be5ca9cf29292ce00566c8ef87184b4
Devr <devrtroll@localhost.localdomain>
2017-11-20 19:19:45 -0600

    coming soon

---
+++ extra/scripts/scriptTemplate.js
@@ -0,0 +1,10 @@
+/*
+  Scriptz-Project: making Rvedy 2 modular. (coming soon) 
+*/
+const author = ("")
+const help = (``)
+// your script here
+
+//end of your script
+exports.author = author
+exports.help = author
