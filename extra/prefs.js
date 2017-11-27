3e93cc826ec9e703f16e9c76181829eb69c0004e
Devr <devrtrolla@gmail.com>
2017-11-26 22:03:20 -0600

    Create prefs.js

---
+++ extra/prefs.js
@@ -0,0 +1,16 @@
+/*
+  Peferences for you the user.
+*/
+const advancedLog = false //show everything that is happening.
+const deleteCommands = true //delete commands requested by you
+const advancedCommands = true //dangerous
+const prefix = ("-")
+// super secret
+const userToken = ("your_token_here")
+
+// end of peferences
+exports.token = userToken
+exports.delComs = advancedCommands
+exports.advLog = advancedLog
+exports.advComs = advancedCommands
+exports.prefix = prefix
