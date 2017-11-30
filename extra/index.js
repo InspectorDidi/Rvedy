2a06263d5ec06cd9a7c0227bac7393d456d25cc1
Devr <devrtroll@localhost.localdomain>
2017-11-30 17:37:08 -0600

    New organization

--- extra/index.js
+++ extra/index.js
@@ -2,7 +2,7 @@
 	const Discord = require("discord.js")	//DiscordJS Module (discordjs.org)
 	const client = new Discord.Client();
 	const rvedy_Version = ('2_17');
-	const seto = require('./prefs.js')
+	const seto = require('../prefs.json')
 
 // Begin
  client.on('message', msg => {
@@ -23,74 +23,74 @@
 	These commands will remain in the index
 	*/
 	try {
-			if (seto.advComs === true) {
+			if (seto.advancedCommands === true) {
 			if (msg.author.id === client.user.id) {
 			if (msg.content.indexOf(seto.prefix+"say ") === 0) {
 		    	msg.channel.send(eval(msg.content.substr(5).toString()))
 		    	report(msg.content)
-		    	if (seto.delComs === true) { msg.delete() }
+		    	if (seto.deleteCommands === true) { msg.delete() }
 			}}}
 	} catch(err){
 		console.log(`[Error]: ${msg.content} - ${err} (${msg.guild})`)
 		msg.reply(err.toString())
-		if (seto.delComs === true) { msg.delete() }
+		if (seto.deleteCommands === true) { msg.delete() }
 	}
 
 	try {
-			if (seto.advComs === true) {
+			if (seto.advancedCommands === true) {
 			if (msg.author.id === client.user.id) {
 			if (msg.content.indexOf(seto.prefix+"console ") === 0) {
 		    	console.log(eval(msg.content.substr(9).toString()))
 		    	report(msg.content)
-		    	if (seto.delComs === true) { msg.delete() }
+		    	if (seto.deleteCommands === true) { msg.delete() }
 			}}}
 	} catch(err){
 		console.log(`[Error]: ${msg.content} - ${err} (${msg.guild})`)
 		msg.reply(err.toString())
-		if (seto.delComs === true) { msg.delete() }
+		if (seto.deleteCommands === true) { msg.delete() }
 	}
 
 	try {
-			if (seto.advComs === true) {
+			if (seto.advancedCommands === true) {
 			if (msg.author.id === client.user.id) {
 			if (msg.content.indexOf(seto.prefix+"reply ") === 0) {
 				msg.reply(eval(msg.content.substr(7).toString()))
 				report('reply '+msg.content.substr(7))
-			if (seto.delComs === true) { msg.delete() }
+			if (seto.deleteCommands === true) { msg.delete() }
 			}}}
 	} catch(err){
 		console.log(`[Error]: ${msg.content} - ${err} (${msg.guild})`)
 		msg.reply(err.toString())
-		if (seto.delComs === true) { msg.delete() }
+		if (seto.deleteCommands === true) { msg.delete() }
 	}
 
 	try {
-			if (seto.advComs === true) {
+			if (seto.advancedCommands === true) {
 			if (msg.author.id === client.user.id) {
 			if (msg.content.indexOf(seto.prefix+"do ") === 0) {
 		    	(eval(msg.content.substr(4).toString()))
 		    	report('do '+msg.content.substr(4))
-			if (seto.delComs === true) { msg.delete() }
+			if (seto.deleteCommands === true) { msg.delete() }
 			}}}
 	} catch(err){
 		console.log(`[Error]: ${msg.content} - ${err} (${msg.guild})`)
 		msg.reply(err.toString())
-		if (seto.delComs === true) { msg.delete() }
+		if (seto.deleteCommands === true) { msg.delete() }
 	}
 
  })
 
 /*
 	Startup Script
-*/
+*/seto.deleteCommands
 client.on('ready', () => {
 	console.log(`[${client.user.username}]: Connected!`)
 })
  client.once('ready', () => {
  	console.log(`[${client.user.username}]: Peferences
 Prefix: ${seto.prefix}
-Delete Commands: ${seto.delComs}
-Advanced Commands: ${seto.advComs}
+Delete Commands: ${seto.deleteCommands}
+Advanced Commands: ${seto.advancedCommands}
 Advanced Logging: ${seto.advLog}`)
 })
 
--- start.sh
+++
@@ -1,1 +0,0 @@
-node './extra/index.js'
